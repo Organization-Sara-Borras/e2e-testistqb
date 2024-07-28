c2101e1064b6bbc3bf7e6872999b5749e3b608f1
Sara <sarabor09@gmail.com>
2024-07-28 11:36:17 +0200

    fix test

--- cypress/e2e/step_definitions/answer_a_question_steps.js
+++ cypress/e2e/step_definitions/answer_a_question_steps.js
@@ -9,7 +9,7 @@ import {
 When ("the user select answer {string} on question {string}", (selectedAnswer,questionnumber)=>{
 
     cy.visit("https://sarabor09.github.io/ISTQB-Online-Test/");
-    cy.get('[data-cy="option'+questionnumber+'"]').select(selectedAnswer);
+    cy.get('[data-cy="option'+selectedAnswer+'"]').check();
 });
 /*
 When ("the user select answer {string} on question {string}", (selectedAnswer,questionnumber)=>{
