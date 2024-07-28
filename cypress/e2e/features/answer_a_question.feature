8328ef6a78e5fef4ee0bd02af79a901310291f03
Sara <sarabor09@gmail.com>
2024-07-28 11:53:02 +0200

    change test

--- cypress/e2e/features/answer_a_question.feature
+++ cypress/e2e/features/answer_a_question.feature
@@ -4,7 +4,5 @@ Scenario: Select a valid answer
 
 Given the student is on the ISTQB exam page
 When the user select answer "option1" on question "1"
-And the user select answer "Respuesta A" on question "2"
-And the user check answer "A" on question "3"
 And the user press correct button on question "1"
-Then the user should see the message "Incorrect", -1 poitns. "on question "1"
+Then the user should see the message "Correct answer 2 points" on question "1"
--- cypress/e2e/step_definitions/answer_a_question_steps.js
+++ cypress/e2e/step_definitions/answer_a_question_steps.js
@@ -10,29 +10,23 @@ When ("the user select answer {string} on question {string}", (selectedAnswer,qu
 
     cy.visit("https://sarabor09.github.io/ISTQB-Online-Test/");
     cy.get('[data-cy="'+selectedAnswer+'"]').check();
-});
-/*
-When ("the user select answer {string} on question {string}", (selectedAnswer,questionnumber)=>{
-
-    cy.visit("https://sarabor09.github.io/ISTQB-Online-Test/");
-    cy.get('[data-cy="question'+questionnumber+'"]').find('input').check(selectedAnswer);
 
 });
-
 
 When ("the user press correct button on question {string}", (questionnumber)=>{
 
     cy.visit("https://sarabor09.github.io/ISTQB-Online-Test/");
-    cy.get('[data-cy="button-correct-question'+questionnumber+'"]').click();
+    cy.get('[data-cy="button'+questionnumber+'"]').click();
     
 });
 
 Then ("The user should see the message {string} on question {string}", (expectedMessage,questionnumber)=>{
 
     cy.visit("https://sarabor09.github.io/ISTQB-Online-Test/");
-    cy.get('data-cy="messagequestion'+questionnumber+'"]').should('have.text',expectedMessage);
+    cy.get('data-cy="answerText'+questionnumber+'"]').should('have.text',expectedMessage);
 
 });
+/*
 
 When ("When the user answer questions", (datatable)=>{
 
