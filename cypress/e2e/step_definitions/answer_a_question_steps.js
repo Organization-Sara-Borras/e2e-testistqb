6c4c8729507bf4ce6d18695e577fa82ecda1a938
Sara <sarabor09@gmail.com>
2024-07-28 12:10:44 +0200

    add step

--- cypress/e2e/step_definitions/answer_a_question_steps.js
+++ cypress/e2e/step_definitions/answer_a_question_steps.js
@@ -2,25 +2,30 @@ import {
     Given,
     When,
     And,
-    Then,
+    Then
   } from "@badeball/cypress-cucumber-preprocessor";
 
 //const { find } = require("cypress/types/lodash");
 
-When ("the user select answer {string} on question {string}", (selectedAnswer,questionnumber)=>{
+Given("the user is on the ISTQB exam page", () => {
 
     cy.visit("https://sarabor09.github.io/ISTQB-Online-Test/");
+
+});
+
+When("the user select answer {string} on question {string}", (selectedAnswer,questionnumber)=>{
+
     cy.get('[data-cy="'+selectedAnswer+'"]').check();
 
 });
 
-And ("the user press correct button on question {string}", (questionnumber)=>{
+And("the user press correct button on question {string}", (questionnumber)=>{
 
     cy.get('[data-cy="button'+questionnumber+'"]').click();
     
 });
 
-Then ("The user should see the message {string} on question {string}", (expectedMessage,questionnumber)=>{
+Then("the user should see the message {string} on question {string}", (expectedMessage,questionnumber)=>{
 
     cy.get('data-cy="answerText'+questionnumber+'"]').should('have.text',expectedMessage);
 
